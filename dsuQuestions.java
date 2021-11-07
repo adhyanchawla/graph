8f861bb9a6db1f177dd6c6aa9146bce331128e8e
imadhyan <adhyanchawla500@gmail.com>
2021-11-07 09:12:37 +0530

    commit

--- dsuQuestions.java
+++ dsuQuestions.java
@@ -296,5 +296,45 @@ public class dsuQuestions {
         return cost <= k ? superRoads : -1;
     }
 
-    
+    //lc 959 regions cut by slashes
+    public int regionsBySlashes(String[] grid) {
+        int n = grid.length;
+        if(n == 0) return 0;
+        int m = n + 1;
+        
+        par = new int[m * m];
+        
+        for(int i = 0; i < m * m; i++) {
+            par[i] = i;
+            int r = i / m;
+            int c = i % m;
+            if(r == 0 || c == 0 || r == m - 1 || c == m - 1) {
+                par[i] = 0;
+            }
+        }
+        
+        
+        int region = 1;
+        for(int i = 0; i < n; i++) {
+            String s = grid[i];
+            for(int j = 0; j < s.length(); j++) {
+                char ch = s.charAt(j);
+                int p1 = 0, p2 = 0;
+                if(ch == '/') {
+                    p1 = findPar((i + 1) * m + j);
+                    p2 = findPar(i *m + (j + 1));
+                } else if(ch == '\\') {
+                    p1 = findPar(i*m + j);
+                    p2 = findPar((i + 1) * m + (j + 1));
+                } else continue;
+                
+                if(p1 != p2) {
+                    par[p1] = p2;
+                } else region++;
+            }
+        }
+        return region;
+    }
+
+
 }
